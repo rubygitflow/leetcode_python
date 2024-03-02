5f43d7c7bc5413a3af1a72c4a6a2b561a1eb9eb6
rubygitflow <nrubygitflow@gmail.com>
2024-03-03 00:32:06 +0600

    add Find All Anagrams in a String

--- valid_anagram.py
+++ valid_anagram.py
@@ -41,8 +41,36 @@
 # 1 <= s.length, t.length <= 5 * 104
 # s and t consist of lowercase English letters.
 
+##################
+# https://leetcode.com/problems/find-all-anagrams-in-a-string/description/
+# 438. Find All Anagrams in a String
+
+# Given two strings s and p, return an array of all the start indices of p's anagrams in s. You may return the answer in any order.
+# An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+# Example 1:
+# Input: s = "cbaebabacd", p = "abc"
+# Output: [0,6]
+# Explanation:
+# The substring with start index = 0 is "cba", which is an anagram of "abc".
+# The substring with start index = 6 is "bac", which is an anagram of "abc".
+
+# Example 2:
+# Input: s = "abab", p = "ab"
+# Output: [0,1,2]
+
+# Explanation:
+# The substring with start index = 0 is "ab", which is an anagram of "ab".
+# The substring with start index = 1 is "ba", which is an anagram of "ab".
+# The substring with start index = 2 is "ab", which is an anagram of "ab".
+ 
+# Constraints:
+# 1 <= s.length, p.length <= 3 * 104
+# s and p consist of lowercase English letters.
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        ''' Valid Anagram '''
         if len(s) != len(t):
             return False
         dic_s = {}
@@ -57,6 +85,7 @@ class Solution:
             dic_t[char] += 1
         return dic_s == dic_t
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ''' Group Anagrams '''
         output = [[]]
         if not strs:
             return output
@@ -71,6 +100,13 @@ class Solution:
             if not found:
                 output.append([strs[i + 1]])
         return output
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        ''' Find All Anagrams in a String '''
+        output = []
+        for i in range(len(s) - len(p) + 1):
+            if self.isAnagram(s[i:i + len(p)], p):
+                output.append(i)
+        return output
 
 s, t = "anagram", "naGaram"
 # s, t = "rat", "car"
@@ -82,3 +118,8 @@ Solution().isAnagram(s, t)
 strs = ["eat","tea","tan","ate","nat","bat"]
 
 Solution().groupAnagrams(strs)
+
+# s, p = "abab", "ab"
+s, p = "cbaebabacd", "abc"
+
+Solution().findAnagrams(s, p)
