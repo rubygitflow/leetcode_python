4c7c35c49bd1d2b1b1f7133d5b1e4096227ad5ff
rubygitflow <nrubygitflow@gmail.com>
2024-03-04 03:44:24 +0600

    add binary vector tasks

---
+++ binary_vector.py
@@ -0,0 +1,48 @@
+# find the maximum length of a sequence of Ones in a binary vector
+# найти максимальную длину последовательности из единиц в бинарном векторе 
+
+def longOne(binary_list):
+    maxLength = 0
+    length = 0
+    for i in binary_list:
+        if i == 1:
+            length += 1
+            maxLength = max(maxLength, length)
+        else:
+            length = 0
+    return maxLength
+
+longOne([0,0,0,1,1,0,1,0,1,0,1,1,1,1,0,1])
+# Output: 4 
+longOne([0,0,0,1,1,0,1,0,1,1,1,1,1,1,0,1])
+# Output: 6
+longOne([0,0,0,0,])
+# Output: 0
+
+###################
+# find all sequences of Ones in a binary vector
+# найти все последовательности из единиц в бинарном векторе 
+def allOne(binary_list):
+    output = {}
+    left = 0
+    length = 0
+    for i, n in enumerate(binary_list):
+        if n == 1:
+            if length == 0:
+                left = i
+                output[left] = 1
+            else:
+                output[left] += 1
+            length = output[left]
+        else:
+            length = 0
+    return output
+
+allOne([0,0,0,1,1,0,1,0,1,0,1,1,1,1,0,1])
+# Output: {3: 2, 6: 1, 8: 1, 10: 4, 15: 1}
+allOne([0,0,0,1,1,0,1,0,1,1,1,1,1,1,0,1])
+# Output: {3: 2, 6: 1, 8: 6, 15: 1}
+allOne([0,0,0,0,])
+# Output: {}
+allOne([1,1,1,1,])
+# Output: {0: 4}
