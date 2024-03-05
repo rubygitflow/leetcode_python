02e6139ef00abad7def97d9fe4d6ea7694bb5cce
rubygitflow <nrubygitflow@gmail.com>
2024-03-05 22:20:59 +0600

    add Sliding Window Maximum

--- sliding_window_median.py
+++ sliding_window_median.py
@@ -28,6 +28,35 @@
 # 1 <= k <= nums.length <= 105
 # -231 <= nums[i] <= 231 - 1
 
+#######################
+# https://leetcode.com/problems/sliding-window-maximum/description/
+# 239. Sliding Window Maximum
+
+# You are given an array of integers nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position.
+# Return the max sliding window.
+
+# Example 1:
+# Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
+# Output: [3,3,5,5,6,7]
+# Explanation: 
+# Window position                Max
+# ---------------               -----
+# [1  3  -1] -3  5  3  6  7       3
+#  1 [3  -1  -3] 5  3  6  7       3
+#  1  3 [-1  -3  5] 3  6  7       5
+#  1  3  -1 [-3  5  3] 6  7       5
+#  1  3  -1  -3 [5  3  6] 7       6
+#  1  3  -1  -3  5 [3  6  7]      7
+
+# Example 2:
+# Input: nums = [1], k = 1
+# Output: [1]
+
+# Constraints:
+# 1 <= nums.length <= 105
+# -104 <= nums[i] <= 104
+# 1 <= k <= nums.length
+
 from typing import List
 
 class Solution:
@@ -51,7 +80,22 @@ class Solution:
             median = (window[k // 2] + window[k // 2 - 1]) / 2
         res.append(float(median))
         return res
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        res = []
+        window = nums[:k]
+        for i in range(k, len(nums)):
+            res.append(max(window))
+            window.remove(nums[i - k])
+            window.append(nums[i])
+        # Process the last window
+        res.append(max(window))
+        return res
 
 nums, size = [1,3,-1,-3,5,3,6,7], 3
 # Output: [1.00000,-1.00000,-1.00000,3.00000,5.00000,6.00000]
 Solution().medianSlidingWindow(nums, size)
+
+nums, size = [1,3,-1,-3,5,3,6,7], 3
+# Output: [3,3,5,5,6,7]
+Solution().maxSlidingWindow(nums, size)
+
