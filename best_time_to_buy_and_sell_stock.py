7257c7c7dafa2cada84d93adb2059e17189c1bec
rubygitflow <nrubygitflow@gmail.com>
2024-03-06 08:34:00 +0600

    add Best Time to Buy and Sell Stock III

--- best_time_to_buy_and_sell_stock.py
+++ best_time_to_buy_and_sell_stock.py
@@ -50,6 +50,35 @@
 # 1 <= prices.length <= 3 * 104
 # 0 <= prices[i] <= 104
 
+#######################
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/description/
+# 123. Best Time to Buy and Sell Stock III
+
+# You are given an array prices where prices[i] is the price of a given stock on the ith day.
+# Find the maximum profit you can achieve. You may complete at most two transactions.
+# Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
+
+# Example 1:
+# Input: prices = [3,3,5,0,0,3,1,4]
+# Output: 6
+# Explanation: Buy on day 4 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
+# Then buy on day 7 (price = 1) and sell on day 8 (price = 4), profit = 4-1 = 3.
+
+# Example 2:
+# Input: prices = [1,2,3,4,5]
+# Output: 4
+# Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+# Note that you cannot buy on day 1, buy on day 2 and sell them later, as you are engaging multiple transactions at the same time. You must sell before buying again.
+
+# Example 3:
+# Input: prices = [7,6,4,3,1]
+# Output: 0
+# Explanation: In this case, no transaction is done, i.e. max profit = 0.
+
+# Constraints:
+# 1 <= prices.length <= 105
+# 0 <= prices[i] <= 105
+
 from typing import List
 
 class Solution:
@@ -70,6 +99,18 @@ class Solution:
     def maxProfitII(self, prices: List[int]) -> int:
         ''' Best Time to Buy and Sell Stock II '''
         return sum( [prices[i] - prices[i - 1] for i in range(1, len(prices)) if prices[i] > prices[i - 1]] )
+    def maxProfitIII(self, prices: List[int]) -> int:
+        ''' Best Time to Buy and Sell Stock III '''
+        deals = [[prices[i - 1], prices[i]] for i in range(1, len(prices)) if prices[i] > prices[i - 1]]
+        if not deals: return 0
+        merged_deals = [deals[0]]
+        for i in range(1, len(deals)):
+            if deals[i][0] == merged_deals[-1][1]:
+                merged_deals[-1][1] = deals[i][1]
+            else:
+                merged_deals.append(deals[i])
+        profits = [deal[1] - deal[0] for deal in merged_deals]
+        return sum(sorted(profits)[-2:])
 
 prices = [7,1,5,3,6,4]
 # Output: 5
@@ -82,3 +123,7 @@ prices = [7,1,5,3,6,4]
 prices = [1,2,3,4,5]
 # Output: 4
 Solution().maxProfitII(prices)
+
+prices = [3,3,5,0,0,3,1,4]
+# Output: 6
+Solution().maxProfitIII(prices)
