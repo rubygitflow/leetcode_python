3eb6e9a2814a1b6624c535d2c64c485ff41756f5
rubygitflow <nrubygitflow@gmail.com>
2024-03-08 01:00:25 +0600

    add Linked List Cycle II

--- linked_list_cycle.py
+++ linked_list_cycle.py
@@ -74,17 +74,49 @@
 
 # Follow up: A linked list can be reversed either iteratively or recursively. Could you implement both?
 
+##########################
+# https://leetcode.com/problems/linked-list-cycle-ii/description/
+# 142. Linked List Cycle II
+
+# Given the head of a linked list, return the node where the cycle begins. If there is no cycle, return null.
+# There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to (0-indexed). It is -1 if there is no cycle. Note that pos is not passed as a parameter.
+# Do not modify the linked list.
+
+# Example 1:
+# Input: head = [3,2,0,-4], pos = 1
+# Output: tail connects to node index 1
+# Explanation: There is a cycle in the linked list, where tail connects to the second node.
+# Example 2:
+# Input: head = [1,2], pos = 0
+# Output: tail connects to node index 0
+# Explanation: There is a cycle in the linked list, where tail connects to the first node.
+# Example 3:
+# Input: head = [1], pos = -1
+# Output: no cycle
+# Explanation: There is no cycle in the linked list.
+
+# Constraints:
+# The number of the nodes in the list is in the range [0, 104].
+# -105 <= Node.val <= 105
+# pos is -1 or a valid index in the linked-list.
+
+# Follow up: Can you solve it using O(1) (i.e. constant) memory?
+
 # Definition for singly-linked list.
 class ListNode:
     def __init__(self, x):
         self.val = x
         self.next = None
+    def __repr__(self) -> str:
+        return f"val: {self.val}, next: {self.next}"
+    def __str__(self) -> str:
+        return str(self.val)
 
 from typing import Optional
 
 class Solution:
     def hasCycleTwoPointer(self, head: Optional[ListNode]) -> bool:
-        ''' Code Two-Pointer '''
+        ''' Linked List Cycle. Two-Pointer '''
         slow_pointer = head
         fast_pointer = head
         while fast_pointer and fast_pointer.next:
@@ -94,7 +126,7 @@ class Solution:
                 return True
         return False
     def hasCycleHashTable(self, head: Optional[ListNode]) -> bool:
-        ''' Code Hash Table '''
+        ''' Linked List Cycle. Hash Table '''
         visited_nodes = set()
         current_node = head
         while current_node:
@@ -157,7 +189,28 @@ class Solution:
             except:
                 print('-current|val      from Next_node',current)
         return new_list
-
+    def detectCycle(self, head: ListNode) -> ListNode:
+        ''' Linked List Cycle II '''
+        # Initialize two pointers, slow and fast, to the head of the linked list.
+        slow = head
+        fast = head
+        # Move the slow pointer one step and the fast pointer two steps at a time through the linked list,
+        # until they either meet or the fast pointer reaches the end of the list.
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                # If the pointers meet, there is a cycle in the linked list.
+                # Reset the slow pointer to the head of the linked list, and move both pointers one step at a time
+                # until they meet again. The node where they meet is the starting point of the cycle.
+                slow = head
+                while slow != fast:
+                    slow = slow.next
+                    fast = fast.next
+                return slow
+        # If the fast pointer reaches the end of the list without meeting the slow pointer,
+        # there is no cycle in the linked list. Return None.
+        return None
 
 def add_linked_list(data, pos = -1):
     tail = []
@@ -202,3 +255,14 @@ res = Solution().reverseListVis(add_linked_list(head))
 # ...
 res.val
 res = res.next
+
+#########
+head = add_linked_list([3,2,0,-4], 1)
+# Output: tail connects to node index 1
+head = add_linked_list([1,2], 0)
+# Output: tail connects to node index 0
+head = add_linked_list([1], -1)
+# Output: no cycle
+res = Solution().detectCycle(head)
+# ...
+res
