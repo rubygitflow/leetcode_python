978cc5f44cb5f647540ab73ccd8c12cf672733f9
rubygitflow <nrubygitflow@gmail.com>
2024-03-07 20:48:08 +0600

    add Find Minimum in Rotated Sorted Array

--- search_in_rotated_sorted_array.py
+++ search_in_rotated_sorted_array.py
@@ -48,6 +48,39 @@
 # nums is guaranteed to be rotated at some pivot.
 # -104 <= target <= 104
 
+########################
+# https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/description/
+# 153. Find Minimum in Rotated Sorted Array
+
+# Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0,1,2,4,5,6,7] might become:
+# [4,5,6,7,0,1,2] if it was rotated 4 times.
+# [0,1,2,4,5,6,7] if it was rotated 7 times.
+# Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
+# Given the sorted rotated array nums of unique elements, return the minimum element of this array.
+# You must write an algorithm that runs in O(log n) time.
+
+# Example 1:
+# Input: nums = [3,4,5,1,2]
+# Output: 1
+# Explanation: The original array was [1,2,3,4,5] rotated 3 times.
+
+# Example 2:
+# Input: nums = [4,5,6,7,0,1,2]
+# Output: 0
+# Explanation: The original array was [0,1,2,4,5,6,7] and it was rotated 4 times.
+
+# Example 3:
+# Input: nums = [11,13,15,17]
+# Output: 11
+# Explanation: The original array was [11,13,15,17] and it was rotated 4 times. 
+
+# Constraints:
+# n == nums.length
+# 1 <= n <= 5000
+# -5000 <= nums[i] <= 5000
+# All the integers of nums are unique.
+# nums is sorted and rotated between 1 and n times.
+
 from typing import List
 
 class Solution:
@@ -97,6 +130,24 @@ class Solution:
                 else:
                     r = mid - 1
         return False
+    def findMin(self, nums: List[int]) -> int:
+        ''' Find Minimum in Rotated Sorted Array '''
+        if not nums:
+            return None
+        if len(nums) == 1 or nums[0] < nums[len(nums) - 1]:
+            return nums[0]
+        l, r = 0, len(nums) - 1
+        border = nums[0]
+        output = border
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] > border:
+                l = mid + 1
+            else:
+                r = mid - 1
+                output = min(output, nums[mid])
+        return output
+
 
 nums, target = [4,5,6,7,0,1,2], 0
 # Output: 4
@@ -111,3 +162,11 @@ nums, target = [2,5,6,0,0,1,2], 0
 nums, target = [2,5,6,0,0,1,2], 3
 # Output: false
 Solution().searchII(nums, target)
+
+nums = [3,4,5,1,2]
+# Output: 1
+nums = [4,5,6,7,0,1,2]
+# Output: 0
+nums = [11,13,15,17]
+# Output: 11
+Solution().findMin(nums)
