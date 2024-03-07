d49efdb2c6d0b57cad39f659d4e24f114ca02938
rubygitflow <nrubygitflow@gmail.com>
2024-03-08 00:08:36 +0600

    add Binary Tree Maximum Path Sum

--- same_tree.py
+++ same_tree.py
@@ -121,6 +121,28 @@
 # -1000 <= Node.val <= 1000
 # -1000 <= targetSum <= 1000
 
+#######################
+# https://leetcode.com/problems/binary-tree-maximum-path-sum/description/
+# 124. Binary Tree Maximum Path Sum
+
+# A path in a binary tree is a sequence of nodes where each pair of adjacent nodes in the sequence has an edge connecting them. A node can only appear in the sequence at most once. Note that the path does not need to pass through the root.
+# The path sum of a path is the sum of the node's values in the path.
+# Given the root of a binary tree, return the maximum path sum of any non-empty path.
+
+# Example 1:
+# Input: root = [1,2,3]
+# Output: 6
+# Explanation: The optimal path is 2 -> 1 -> 3 with a path sum of 2 + 1 + 3 = 6.
+
+# Example 2:
+# Input: root = [-10,9,20,None,None,15,7]
+# Output: 42
+# Explanation: The optimal path is 15 -> 20 -> 7 with a path sum of 15 + 20 + 7 = 42.
+
+# Constraints:
+# The number of nodes in the tree is in the range [1, 3 * 104].
+# -1000 <= Node.val <= 1000
+
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -211,6 +233,19 @@ class Solution:
         #Calling our dfs method and passing root as main node and sum(summa) as 0
         dfs(root, 0)
         return output
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        ''' Binary Tree Maximum Path Sum '''
+        def dfs(root: Optional[TreeNode]) -> int:
+            if root is None:
+                return 0
+            left = dfs(root.left)
+            right = dfs(root.right)
+            nonlocal ans
+            ans = max(ans, root.val + left + right)
+            return root.val
+        ans = float('-inf')
+        dfs(root)
+        return ans
 
 def build_binary_tree(items: list[int]) -> TreeNode:
     """Create BT from list of values."""
@@ -274,3 +309,9 @@ p, targetSum = [1,2,3], 5
 p, targetSum = [1,2], 0
 # Output: []
 Solution().pathSum(build_binary_tree(p), targetSum)
+
+p = [1,2,3]
+# Output: 6
+p = [-10,9,20,None,None,15,7]
+# Output: 42
+Solution().maxPathSum(build_binary_tree(p))
