e09d7c2444d02923f1a099edf44c060cb649da9f
rubygitflow <nrubygitflow@gmail.com>
2024-03-12 20:03:46 +0600

    add Lonely Pixel II

--- lonely_pixel.py
+++ lonely_pixel.py
@@ -19,6 +19,35 @@
 # 1 <= m, n <= 500
 # picture[i][j] is 'W' or 'B'.
 
+
+#######################
+# https://leetcode.com/problems/lonely-pixel-ii/description/
+# 533 - Lonely Pixel II
+
+# Given an m x n picture consisting of black 'B' and white 'W' pixels and an integer target, return the number of black lonely pixels.
+# A black lonely pixel is a character 'B' that located at a specific position (r, c) where:
+# Row r and column c both contain exactly target black pixels.
+# For all rows that have a black pixel at column c, they should be exactly the same as row r.
+
+# Example 1:
+# Input: picture = [["W","B","W","B","B","W"],["W","B","W","B","B","W"],["W","B","W","B","B","W"],["W","W","B","W","B","W"]], target = 3
+# Output: 6
+# Explanation: All the green 'B' are the black pixels we need (all 'B's at column 1 and 3).
+# Take 'B' at row r = 0 and column c = 1 as an example:
+#  - Rule 1, row r = 0 and column c = 1 both have exactly target = 3 black pixels. 
+#  - Rule 2, the rows have black pixel at column c = 1 are row 0, row 1 and row 2. They are exactly the same as row r = 0.
+
+# Example 2:
+# Input: picture = [["W","W","B"],["W","W","B"],["W","W","B"]], target = 1
+# Output: 0
+
+# Constraints:
+# m == picture.length
+# n == picture[i].length
+# 1 <= m, n <= 200
+# picture[i][j] is 'W' or 'B'.
+# 1 <= target <= min(m, n)
+
 from typing import List
 
 class Solution:
@@ -36,6 +65,31 @@ class Solution:
                     else:
                         count += 1
         return count 
+    def findBlackPixel(self, picture: List[List[str]], target: int) -> int:
+        ''' Lonely Pixel II (???) '''
+        m, n = len(picture), len(picture[0])
+        rows = [0] * m
+        cols = defaultdict(list)
+        for i in range(m):
+            for j in range(n):
+                if picture[i][j] == 'B':
+                    rows[i] += 1
+                    cols[j].append(i)
+        t = [[False] * m for _ in range(m)]
+        for i in range(m):
+            for k in range(i, m):
+                if i == k:
+                    t[i][k] = True
+                else:
+                    t[i][k] = all([picture[i][j] == picture[k][j] for j in range(n)])
+                t[k][i] = t[i][k]
+        res = 0
+        for i in range(m):
+            if rows[i] == target:
+                for j in range(n):
+                    if len(cols[j]) == target and all([t[i][k] for k in cols[j]]):
+                        res += 1
+        return res
 
 picture = [["W","W","B"],["W","B","W"],["B","W","W"]]
 # Output: 3
@@ -43,4 +97,11 @@ picture = [["B","B","B"],["B","B","W"],["B","B","B"]]
 # Output: 0
 Solution().findLonelyPixel(picture)
 
+picture, target = [["W","B","W","B","B","W"],["W","B","W","B","B","W"],["W","B","W","B","B","W"],["W","W","B","W","B","W"]], 3
+# Output: 6
+picture, target = [["W","W","B"],["W","W","B"],["W","W","B"]], 1
+# Output: 0
+picture, target = [["W","W","B"],["W","W","W"],["W","W","W"]], 1
+# Output: 1
+Solution().findBlackPixel(picture, target)
 
