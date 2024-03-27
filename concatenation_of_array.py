14b27325d386d55c004341cd2443494fb9275ee5
rubygitflow <nrubygitflow@gmail.com>
2024-03-27 14:48:51 +0600

    add example of concatenation

--- concatenation_of_array.py
+++ concatenation_of_array.py
@@ -28,3 +28,8 @@ from typing import List
 class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
         return nums + nums
+
+print(Solution().getConcatenation([1,2,1]))
+# Output: [1,2,1,1,2,1]
+print(Solution().getConcatenation([1,3,2,1]))
+# Output: [1,3,2,1,1,3,2,1]
