902299f271ef8cc8f10d9819f8ef38ca80b3b09f
rubygitflow <nrubygitflow@gmail.com>
2024-04-10 00:25:50 +0600

    algorithmic options for Largest Time for Given Digits

--- largest_time_for_given_digits.py
+++ largest_time_for_given_digits.py
@@ -19,6 +19,7 @@
 # arr.length == 4
 # 0 <= arr[i] <= 9
 
+from itertools import permutations
 from typing import List
 
 class Solution:
@@ -52,10 +53,51 @@ class Solution:
                 out = hour_first_digit(arr,str(i))
                 break
         return out
+    def largestTimeFromDigitsEx(self, A: List[int]) -> str:
+        ''' Largest Time for Given Digits (with permutations)'''
+        arr = list(permutations(sorted(A, reverse=True)))
+        for h1, h2, m1, m2 in arr:
+            if h1 * 10 + h2 < 24 and m1 * 10 + m2 < 60:
+                return f'{h1}{h2}:{m1}{m2}'
+        return ''
+    def largestTimeFromDigitsBF(self, A: List[int]) -> str:
+        ''' Largest Time for Given Digits (Brute Force)'''
+        # From 23:59 to 00:00 go over every minute of 24 hours. If A meets this requirement, then totaly 24 * 60 minutes. Since using sort during the ongoing judegment process, so the time complexity is low.
+        A.sort()
+        for h in range(23, -1, -1):
+            for m in range(59, -1, -1):
+                t = [h//10, h % 10, m // 10, m % 10]
+                ts = sorted(t)
+                if ts == A:
+                    return str(t[0]) + str(t[1]) +':' + str(t[2]) + str(t[3])
+        return ''
 
+print('Largest Time for Given Digits')
 print(Solution().largestTimeFromDigits([1,5,3,4]))
 # Output: "15:43"
 print(Solution().largestTimeFromDigits([1,2,3,4]))
 # Output: "23:41"
 print(Solution().largestTimeFromDigits([5,5,5,5]))
+# Output: ""
+print(Solution().largestTimeFromDigits([0,9,9,9]))
+# Output: ""
+
+print('Largest Time for Given Digits (with permutations)')
+print(Solution().largestTimeFromDigitsEx([1,5,3,4]))
+# Output: "15:43"
+print(Solution().largestTimeFromDigitsEx([1,2,3,4]))
+# Output: "23:41"
+print(Solution().largestTimeFromDigitsEx([5,5,5,5]))
+# Output: ""
+print(Solution().largestTimeFromDigitsEx([0,9,9,9]))
+# Output: ""
+
+print('Largest Time for Given Digits (Brute Force)')
+print(Solution().largestTimeFromDigitsBF([1,5,3,4]))
+# Output: "15:43"
+print(Solution().largestTimeFromDigitsBF([1,2,3,4]))
+# Output: "23:41"
+print(Solution().largestTimeFromDigitsBF([5,5,5,5]))
+# Output: ""
+print(Solution().largestTimeFromDigitsBF([0,9,9,9]))
 # Output: ""
