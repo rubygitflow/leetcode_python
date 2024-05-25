d752eb6abc7c798a478de7e2e7ad83bb6a907199
rubygitflow <nrubygitflow@gmail.com>
2024-05-26 00:06:26 +0600

    add numberOfCombinationsVerifyEx

--- number_ways_to_separate_numbers.py
+++ number_ways_to_separate_numbers.py
@@ -112,7 +112,47 @@ class Solution:
                         if 2*i-j >= 0 and cmp(2*i-j, i, j-i): dp[i][j] += dp[2*i-j][i]
                         if 2*i-j+1 >= 0 and not cmp(2*i-j+1, i, j-i-1): dp[i][j] += dp[2*i-j+1][i]
         return sum(dp[i][n] for i in range(n)) % MOD
+    def numberOfCombinationsVerifyEx(self, num: str) -> int:
+        # Helper function to compare the lexicographical order of two numbers within 'num'
+        # representing the start indices and their length 'length'.
+        def is_greater_or_equal(start1, start2, length):
+            common_prefix_length = longest_common_prefix[start1][start2]
+            return common_prefix_length >= length or num[start1 + common_prefix_length] >= num[start2 + common_prefix_length]
 
+        num_length = len(num)
+
+        # Pre-compute the longest common prefix (LCP) array.
+        longest_common_prefix = [[0] * (num_length + 1) for _ in range(num_length + 1)]
+        for i in range(num_length - 1, -1, -1):
+            for j in range(num_length - 1, -1, -1):
+                if num[i] == num[j]:
+                    longest_common_prefix[i][j] = 1 + longest_common_prefix[i + 1][j + 1]
+        # print("longest_common_prefix",longest_common_prefix)
+
+        # Dynamic programming table where dp[i][j] represents the number of combinations
+        # of valid integers with length 'j' that end at index 'i - 1'.
+        dp = [[0] * (num_length + 1) for _ in range(num_length + 1)]
+        dp[0][0] = 1
+
+        # Build up the DP table.
+        for i in range(1, num_length + 1):
+            for j in range(1, i + 1):
+                current_value = 0
+                if num[i - j] != '0':  # Skip numbers with leading zero.
+                    if i - j - j >= 0 and is_greater_or_equal(i - j, i - j - j, j):
+                        # The substring is greater or equal to the previous,
+                        # so we can safely append to the previous.
+                        current_value = dp[i - j][j]
+                    else:
+                        # Take the combination counts from the smaller number if present.
+                        current_value = dp[i - j][min(j - 1, i - j)]
+
+                # Update the current dp value including the current value with MOD.
+                # Accumulate with the previous j-1 combinations.
+                dp[i][j] = (dp[i][j - 1] + current_value) % MOD
+
+        # Total number of combinations is the last value in the dp table.
+        return dp[num_length][num_length]
 
 
 print("Number of Ways to Separate Numbers (with a non-increasing series of numbers)")
@@ -159,4 +199,22 @@ print(Solution().numberOfCombinationsVerify("113224345345672356"))
 print(Solution().numberOfCombinationsVerify("1132243453456723562456731925"))
 # Output: ? == 1215
 print(Solution().numberOfCombinationsVerify("113224345345672356245673192583113224345345672356245673192583113224345345672356245673192583113224345345672356245673192583113224345345672356245673192583113224345345672356245673192583113224345345672356245673192583"))
+# Output: ? == 156786812  # counts in a second
+
+print("Number of Ways to Separate Numbers (VerifyEx)")
+print(Solution().numberOfCombinationsVerifyEx("327"))
+# Output: 2
+print(Solution().numberOfCombinationsVerifyEx("094"))
+# Output: 0
+print(Solution().numberOfCombinationsVerifyEx("0"))
+# Output: 0
+print(Solution().numberOfCombinationsVerifyEx("1122"))
+# Output: 1/1111+1/22+1/112+1/13+1/4 == 5
+print(Solution().numberOfCombinationsVerifyEx("11022"))
+# Output: 1/14+1/122+1/5 == 3
+print(Solution().numberOfCombinationsVerifyEx("113224345345672356"))
+# Output: ? == 180
+print(Solution().numberOfCombinationsVerifyEx("1132243453456723562456731925"))
+# Output: ? == 1215
+print(Solution().numberOfCombinationsVerifyEx("113224345345672356245673192583113224345345672356245673192583113224345345672356245673192583113224345345672356245673192583113224345345672356245673192583113224345345672356245673192583113224345345672356245673192583"))
 # Output: ? == 156786812  # counts in a second
