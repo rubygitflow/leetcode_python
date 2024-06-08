83c63c7031394d9657b6c1942aeb53b2bb5a7d84
rubygitflow <nrubygitflow@gmail.com>
2024-06-09 03:37:14 +0600

    add a test case

--- minimum_bit_flips.py
+++ minimum_bit_flips.py
@@ -6,9 +6,25 @@ class Solution:
         ''' Minimum Bit Flips to Convert Number '''
         return bin(start ^ goal).count('1')
 
+    def minBitFlipsII(self, start: int, goal: int) -> int:
+        ''' Minimum Bit Flips to Convert Number II'''
+        tmp = start ^ goal
+        out = 0
+        while tmp:
+            out += tmp & 1
+            tmp >>= 1
+        return out
+
 print(Solution().minBitFlips(10, 7))
 # Output: 3
 print(Solution().minBitFlips(3, 4))
 # Output: 3
 print(Solution().minBitFlips(10**9, 1))
+# Output: 14
+
+print(Solution().minBitFlipsII(10, 7))
+# Output: 3
+print(Solution().minBitFlipsII(3, 4))
+# Output: 3
+print(Solution().minBitFlipsII(10**9, 1))
 # Output: 14
