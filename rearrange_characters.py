61acc65339b1156d6431276662c241e4ddc7a7dc
rubygitflow <nrubygitflow@gmail.com>
2024-06-11 05:04:47 +0600

    add Sum of Unique Elements

--- rearrange_characters.py
+++ rearrange_characters.py
@@ -33,7 +33,34 @@
 # 1 <= target.length <= 10
 # s and target consist of lowercase English letters.
 
+########################
+# https://leetcode.com/problems/sum-of-unique-elements/
+# 1748. Sum of Unique Elements
+
+# You are given an integer array nums. The unique elements of an array are the elements that appear exactly once in the array.
+# Return the sum of all the unique elements of nums.
+
+# Example 1:
+# Input: nums = [1,2,3,2]
+# Output: 4
+# Explanation: The unique elements are [1,3], and the sum is 4.
+
+# Example 2:
+# Input: nums = [1,1,1,1,1]
+# Output: 0
+# Explanation: There are no unique elements, and the sum is 0.
+
+# Example 3:
+# Input: nums = [1,2,3,4,5]
+# Output: 15
+# Explanation: The unique elements are [1,2,3,4,5], and the sum is 15.
+
+# Constraints:
+# 1 <= nums.length <= 100
+# 1 <= nums[i] <= 100
+
 from collections import Counter
+from typing import List
 
 class Solution:
     def rearrangeCharacters(self, s: str, target: str) -> int:
@@ -42,6 +69,12 @@ class Solution:
         cnt2 = Counter(target)
         return min(cnt1[c] // v for c, v in cnt2.items())
 
+    def sumOfUnique(self, nums: List[int]) -> int:
+        ''' Sum of Unique Elements '''
+        cnt = Counter(nums)
+        return sum(k for k, v in cnt.items() if v == 1)
+
+print("Rearrange Characters to Make Target String")
 print(Solution().rearrangeCharacters("ilovecodingonleetcode","code"))
 # Output: 2
 print(Solution().rearrangeCharacters("abcba","abc"))
@@ -50,4 +83,11 @@ print(Solution().rearrangeCharacters("abbaccaddaeea","aaaaa"))
 # Output: 1
 print(Solution().rearrangeCharacters("abbaccaddaeea","фd"))
 # Output: 0
-       
+
+print("Sum of Unique Elements")
+print(Solution().sumOfUnique([1,2,3,2]))
+# Output: 4
+print(Solution().sumOfUnique([1,1,1,1,1]))
+# Output: 0
+print(Solution().sumOfUnique([1,2,3,4,5]))
+# Output: 15
