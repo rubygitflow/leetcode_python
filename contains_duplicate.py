09e06e032eb33bc65f6785ed61ec50b075ba6e76
rubygitflow <nrubygitflow@gmail.com>
2024-06-15 20:50:25 +0600

    add Contains Duplicate III

--- contains_duplicate.py
+++ contains_duplicate.py
@@ -44,6 +44,38 @@
 # 0 <= k <= 105
 
 
+####################
+# https://leetcode.com/problems/contains-duplicate-iii/description/
+# 220. Contains Duplicate III
+
+# You are given an integer array nums and two integers indexDiff and valueDiff.
+# Find a pair of indices (i, j) such that:
+# i != j,
+# abs(i - j) <= indexDiff.
+# abs(nums[i] - nums[j]) <= valueDiff, and
+# Return true if such pair exists or false otherwise.
+
+# Example 1:
+# Input: nums = [1,2,3,1], indexDiff = 3, valueDiff = 0
+# Output: true
+# Explanation: We can choose (i, j) = (0, 3).
+# We satisfy the three conditions:
+# i != j --> 0 != 3
+# abs(i - j) <= indexDiff --> abs(0 - 3) <= 3
+# abs(nums[i] - nums[j]) <= valueDiff --> abs(1 - 1) <= 0
+
+# Example 2:
+# Input: nums = [1,5,9,1,5,9], indexDiff = 2, valueDiff = 3
+# Output: false
+# Explanation: After trying all the possible pairs (i, j), we cannot satisfy the three conditions, so we return false.
+
+# Constraints:
+# 2 <= nums.length <= 105
+# -109 <= nums[i] <= 109
+# 1 <= indexDiff <= nums.length
+# 0 <= valueDiff <= 109
+
+
 from typing import List
 
 class Solution:
@@ -60,12 +92,34 @@ class Solution:
 
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
         ''' Contains Duplicate II '''
-        data = dict()
+        visited = dict()
         for i, num in enumerate(nums):
-            if num in data and i - data[num] <= k:
+            if num in visited and i - visited[num] <= k:
                 return True
-            data[num] = i
+
+            visited[num] = i
         return False
+
+    def containsNearbyAlmostDuplicate(self, nums: List[int], indexDiff: int, valueDiff: int) -> bool:
+        ''' Contains Duplicate III '''
+        if len(nums) < 2 or valueDiff < 0 or indexDiff < 1: return False
+
+        nearby = valueDiff + 1
+        visited = dict()
+        for i, num in enumerate(nums):
+            idx = num//nearby
+            # because of rounded idx we almost verify idx-1 and idx+1
+            if (idx in visited and i - visited[idx][0] <= indexDiff) or \
+               (idx-1 in visited and \
+                i - visited[idx-1][0] <= indexDiff and \
+                abs(num - visited[idx-1][1]) <= valueDiff) or \
+               (idx+1 in visited and \
+                i - visited[idx+1][0] <= indexDiff and \
+                abs(num - visited[idx+1][1]) <= valueDiff): return True
+
+            visited[idx] = (i, num)
+        return False
+
 
 print("Contains Duplicate (set)")
 print(Solution().containsDuplicate([1,2,3,1]))
@@ -81,4 +135,10 @@ print(Solution().containsNearbyDuplicate([1,2,3,1], 3))
 print(Solution().containsNearbyDuplicate([1,0,1,1], 1))
 # Output: True
 print(Solution().containsNearbyDuplicate([1,2,3,1,2,3], 2))
+# Output: False
+
+print("Contains Duplicate III")
+print(Solution().containsNearbyAlmostDuplicate([1,2,3,1], 3, 0))
+# Output: True
+print(Solution().containsNearbyAlmostDuplicate([1,5,9,1,5,9], 2, 3))
 # Output: False
