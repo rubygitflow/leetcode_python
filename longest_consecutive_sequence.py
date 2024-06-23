bcc69c48b86fb18742ad59a0b2b0ecb552c2fba6
rubygitflow <nrubygitflow@gmail.com>
2024-06-24 05:30:04 +0600

    update the test cases

--- longest_consecutive_sequence.py
+++ longest_consecutive_sequence.py
@@ -34,10 +34,9 @@ class Solution:
                 max_arr = max(max_arr, y - x)
         return max_arr
 
-nums = [100,4,200,1,3,2]
+print(Solution().longestConsecutive([100,4,200,1,3,2]))
 # Output: 4
-nums = [0,3,7,2,5,8,4,6,0,1]
+print(Solution().longestConsecutive([0,3,7,2,5,8,4,6,0,1]))
 # Output: 9
-nums = [0,3,17,12,5,8,14,26,0,21]
+print(Solution().longestConsecutive([0,3,17,12,5,8,14,26,0,21]))
 # Output: 1
-Solution().longestConsecutive(nums)
