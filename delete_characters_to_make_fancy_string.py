965460939d275d0e350dc70c94da4ed36932687c
rubygitflow <nrubygitflow@gmail.com>
2024-07-15 19:54:40 +0600

    refactor the tests

--- delete_characters_to_make_fancy_string.py
+++ delete_characters_to_make_fancy_string.py
@@ -33,7 +33,7 @@ from functools import reduce
 
 class Solution:
     def makeFancyString(self, s: str) -> str:
-        ''' Delete Characters to Make Fancy String '''
+        ''' Delete Characters to Make Fancy String (FOR-loop)'''
         output = ''
         for c in s:
             if len(output) > 1 and output[-1] == output[-2] == c:
@@ -45,20 +45,23 @@ class Solution:
         ''' Delete Characters to Make Fancy String (reduce)'''
         return reduce(lambda acc, ch: (acc[0] if ch == acc[1] and ch == acc[2] else acc[0] + ch, ch, acc[1]), list(s), ('','',''))[0]
 
-s = "leeetcode"
-print(Solution().makeFancyString(s))
-# Output: "leetcode"
-s = "aaabaaaa"
-print(Solution().makeFancyString(s))
-# Output: "aabaa"
-print(Solution().makeFancyString("aab"))
-# Output: "aab"
+if __name__ == "__main__":
+    print("Delete Characters to Make Fancy String (FOR-loop)")
+    s = "leeetcode"
+    print(Solution().makeFancyString(s))
+    # Output: "leetcode"
+    s = "aaabaaaa"
+    print(Solution().makeFancyString(s))
+    # Output: "aabaa"
+    print(Solution().makeFancyString("aab"))
+    # Output: "aab"
 
-s = "leeetcode"
-print(Solution().makeFancyStringII(s))
-# Output: "leetcode"
-s = "aaabaaaa"
-print(Solution().makeFancyStringII(s))
-# Output: "aabaa"
-print(Solution().makeFancyStringII("aab"))
-# Output: "aab"
+    print("Delete Characters to Make Fancy String (reduce)")
+    s = "leeetcode"
+    print(Solution().makeFancyStringII(s))
+    # Output: "leetcode"
+    s = "aaabaaaa"
+    print(Solution().makeFancyStringII(s))
+    # Output: "aabaa"
+    print(Solution().makeFancyStringII("aab"))
+    # Output: "aab"
--- test_full.py
+++ test_full.py
@@ -1,47 +1,61 @@
+# https://docs.pytest.org/en/latest/
+# https://pypi.org/project/pytest-assertcount/
+
 import pytest
 
 from collect_all_apples_in_tree import Solution as Solution1443
 from single_number import Solution as Solution136_137
 from top_k_frequent_words import Solution as Solution347_692
+from delete_characters_to_make_fancy_string import Solution as Solution1957
+
+def test_the_delete_characters_to_make_fancy_string_forloop():
+    assert Solution1957().makeFancyString("leeetcode") == "leetcode"
+    assert Solution1957().makeFancyString("aaabaaaa") == "aabaa"
+    assert Solution1957().makeFancyString("aab") == "aab"
+
+def test_the_delete_characters_to_make_fancy_string_reduce():
+    assert Solution1957().makeFancyStringII("leeetcode") == "leetcode"
+    assert Solution1957().makeFancyStringII("aaabaaaa") == "aabaa"
+    assert Solution1957().makeFancyStringII("aab") == "aab"
 
 def test_the_top_k_frequent_words():
-    assert ["i","love"] == Solution347_692().topKFrequentWords(["i","love","leetcode","i","love","coding"], 2)
-    assert ["the","is","sunny","day"] == Solution347_692().topKFrequentWords(["the","day","is","sunny","the","the","the","sunny","is","is"], 4)
-    assert ["is", "the"] == Solution347_692().topKFrequentWords(["the","the","the","day","sunny","sunny","is","is","is"], 2)
-    assert ["is", "the", "sunny", "day"] == Solution347_692().topKFrequentWords(["the","the","the","day","sunny","sunny","is","is","is"], 6)
+    assert Solution347_692().topKFrequentWords(["i","love","leetcode","i","love","coding"], 2) == ["i","love"]
+    assert Solution347_692().topKFrequentWords(["the","day","is","sunny","the","the","the","sunny","is","is"], 4) == ["the","is","sunny","day"]
+    assert Solution347_692().topKFrequentWords(["the","the","the","day","sunny","sunny","is","is","is"], 2) == ["is", "the"]
+    assert Solution347_692().topKFrequentWords(["the","the","the","day","sunny","sunny","is","is","is"], 6) == ["is", "the", "sunny", "day"]
 
 def test_the_top_k_frequent_elements():
-    assert [1,2] == Solution347_692().topKFrequentElements([1,1,1,2,2,3], 2)
-    assert [1] == Solution347_692().topKFrequentElements([1], 1)
-    assert [10, 11] == Solution347_692().topKFrequentElements([10,11,13,25,22,30,42,10], 2)
-    assert [10, 11, 13] == Solution347_692().topKFrequentElements([10,11,13,25,22,30,42,10], 3)
-    assert [10, 11, 13, 22, 25, 30, 42] == Solution347_692().topKFrequentElements([10,11,13,25,22,30,42,10], 0)
-    assert [10, 11] == Solution347_692().topKFrequentElements([10,11,13,25,22,30,42,10], -2)
-    assert [0, 1] == Solution347_692().topKFrequentElements([1,1,1,2,2,3,0,0,0,5,5,5], 2)
-    assert [0, 1] == Solution347_692().topKFrequentElements([1,1,1,2,2,3,5,5,5,0,0,0], 2)
+    assert Solution347_692().topKFrequentElements([1,1,1,2,2,3], 2) == [1,2]
+    assert Solution347_692().topKFrequentElements([1], 1) == [1]
+    assert Solution347_692().topKFrequentElements([10,11,13,25,22,30,42,10], 2) == [10, 11]
+    assert Solution347_692().topKFrequentElements([10,11,13,25,22,30,42,10], 3) == [10, 11, 13]
+    assert Solution347_692().topKFrequentElements([10,11,13,25,22,30,42,10], 0) == [10, 11, 13, 22, 25, 30, 42]
+    assert Solution347_692().topKFrequentElements([10,11,13,25,22,30,42,10], -2) == [10, 11]
+    assert Solution347_692().topKFrequentElements([1,1,1,2,2,3,0,0,0,5,5,5], 2) == [0, 1]
+    assert Solution347_692().topKFrequentElements([1,1,1,2,2,3,5,5,5,0,0,0], 2) == [0, 1]
 
 # Python-case only
 # def test_the_single_number_bf():
-#     assert 99 == Solution136_137().singleNumberBF([0,1,0,1,0,1,99])
-#     assert 0 == Solution136_137().singleNumberBF([0,1,0,1,0,1])
+#     assert Solution136_137().singleNumberBF([0,1,0,1,0,1,99]) == 99
+#     assert Solution136_137().singleNumberBF([0,1,0,1,0,1]) == 0
 
 # Python-case only
 # def test_the_single_number_iii():
-#     assert 1 == Solution136_137().singleNumberIII([2,2,1,1,1])
-#     assert 1 == Solution136_137().singleNumberIII([2,2,1])
-#     assert 4 == Solution136_137().singleNumberIII([4,1,2,1,2])
-#     assert 1 == Solution136_137().singleNumberIII([1])
+#     assert Solution136_137().singleNumberIII([2,2,1,1,1]) == 1
+#     assert Solution136_137().singleNumberIII([2,2,1]) == 1
+#     assert Solution136_137().singleNumberIII([4,1,2,1,2]) == 4
+#     assert Solution136_137().singleNumberIII([1]) == 1
 
 def test_the_single_number():
-    assert 1 == Solution136_137().singleNumber([2,2,1,1,1])
-    assert 1 == Solution136_137().singleNumber([2,2,1])
-    assert 4 == Solution136_137().singleNumber([4,1,2,1,2])
-    assert 1 == Solution136_137().singleNumber([1])
+    assert Solution136_137().singleNumber([2,2,1,1,1]) == 1
+    assert Solution136_137().singleNumber([2,2,1]) == 1
+    assert Solution136_137().singleNumber([4,1,2,1,2]) == 4
+    assert Solution136_137().singleNumber([1]) == 1
 
 def test_the_single_number_ii():
-    assert 3 == Solution136_137().singleNumberII([2,2,3,2])
-    assert 99 == Solution136_137().singleNumberII([0,1,0,1,0,1,99])
-    assert 0 == Solution136_137().singleNumberII([0,1,0,1,0,1])
+    assert Solution136_137().singleNumberII([2,2,3,2]) == 3
+    assert Solution136_137().singleNumberII([0,1,0,1,0,1,99]) == 99
+    assert Solution136_137().singleNumberII([0,1,0,1,0,1]) == 0
 
 def test_the_collect_all_apples_in_tree():
     assert Solution1443().minTime(7,[[0,1],[0,2],[1,4],[1,5],[2,3],[2,6]],[False,False,True,False,True,True,False]) == 8 
